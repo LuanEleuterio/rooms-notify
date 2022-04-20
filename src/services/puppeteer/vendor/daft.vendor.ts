b94ea2a3dcbb90ac8de3ef9a9fa3a87038eef7c1
Luan Eleuterio <luaneleuterio3@hotmail.com>
2022-04-19 23:14:36 -0300

    Add get owner and property informations

--- src/services/puppeteer/vendor/daft.vendor.ts
+++ src/services/puppeteer/vendor/daft.vendor.ts
@@ -36,28 +36,7 @@ export class RoomsService {
     
         await this.getRoomInformation(page, listUrlRooms)
     
-        // let totalRoomsDublin = await page.waitForSelector('.styles__SearchH1-sc-1t5gb6v-3.guZHZl')
-        // totalRoomsDublin = await totalRoomsDublin.evaluate(el => el.textContent)
-        // console.log(totalRoomsDublin)
-    
         await browser.close();
-    }
-
-    async getRoomInformation(page: any, urlRooms: Array<string> ): Promise<void> {
-        for(let urlRoom of urlRooms) {
-            // Desenvolver depois uma solução para acomodações estudantis
-            if( urlRoom.includes("student-ac") ) continue
-            
-            await Promise.all([
-                page.goto(urlRoom),
-                page.waitForNavigation()
-            ])
-    
-            let roomValue = await page.waitForSelector('.TitleBlock__StyledSpan-sc-1avkvav-5.fKAzIL')
-            roomValue = await roomValue.evaluate(el => el.textContent)
-        
-            console.log(roomValue)
-        }
     }
 
     async getListUrlRooms(page: any): Promise<Array<string>> {
@@ -68,6 +47,39 @@ export class RoomsService {
         for(let room of listRooms) roomsUrl = [...roomsUrl, await room.evaluate(el => el.childNodes[0].href)]
 
         return roomsUrl
+    }
+
+    async getRoomInformation(page: any, urlRooms: Array<string> ): Promise<void> {
+        for(let urlRoom of urlRooms) {
+            // Desenvolver depois uma solução para acomodações estudantis
+            if( urlRoom.includes("student-ac") ) continue
+                    
+            await Promise.all([
+                page.goto(urlRoom),
+                page.waitForNavigation()
+            ])
+
+            let roomValue = await page.waitForSelector('.TitleBlock__StyledSpan-sc-1avkvav-5.fKAzIL')
+            roomValue = await roomValue.evaluate(el => el.textContent)
+
+            let roomDescription = await page.waitForSelector('.PropertyPage__StandardParagraph-sc-14jmnho-8.iDlZoD', el => el.textContent)
+            roomDescription = await roomDescription.evaluate(el => el.textContent)
+
+            let propertyAttributes = []
+            let propertyOverview = await page.$$('.PropertyPage__ListLabel-sc-14jmnho-10')
+            for(let property of propertyOverview) propertyAttributes = [...propertyAttributes, await property.evaluate(el => el.parentNode.outerText)]
+                
+            let roomOwner = await page.waitForSelector('.ContactPanel__ImageLabel-sc-18zt6u1-6.laVxzi')
+            roomOwner = await roomOwner.evaluate(el => el.textContent)
+
+            let roomOwnerNumber = await page.$$('.NumberReveal__PhoneNumber-v2noxr-0.cLhApw')
+            if(roomOwnerNumber) for(let ownerNumber of roomOwnerNumber) roomOwnerNumber = await ownerNumber.evaluate(el => el.childNodes[0].textContent)
+            
+            console.log(propertyAttributes)
+            console.log(roomOwner)
+            console.log(roomOwnerNumber)
+            console.log(roomValue)
+        }
     }
 
     genQueryString(field: string, locations: Array<string> ): string{
